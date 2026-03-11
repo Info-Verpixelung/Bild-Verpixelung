2d529fa9357d4b9021d4e6333b43999b843c7253
Iiitzenplitz <lieselau@icloud.com>
2026-03-11 12:47:24 +0100

    comments added to app.py

--- backend/api/app.py
+++ backend/api/app.py
@@ -7,31 +7,37 @@ import time
 import os
 import sys
 
-# Handle PyInstaller temp folder
+# Handle PyInstaller temp folder (make sure paths work no matter if the programm is compiled or not)
 if getattr(sys, 'frozen', False):
     base_path = sys._MEIPASS
 else:
     base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-template_folder = os.path.join(base_path, "templates")
-static_folder = os.path.join(base_path, "static")
+# defines where Flask finds the files neccessary to run the Website 
+template_folder = os.path.join(base_path, "templates") # HTML- page
+static_folder = os.path.join(base_path, "static") # Everything else (pictures, java script...)
 
+# Initialising the Web Server
 app = Flask(
     __name__,
     template_folder=template_folder,
     static_folder=static_folder
 )
 
+# Allows Cross-Origin- Requests (Website acessing another server, here: the API) 
 CORS(app)
 
+#Load the html document when someone opens the page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+#Test point
 @app.route("/health", methods=["GET"])
 def health():
     return "status ok"
 
+# Definition of the two APIs (?): detect and censor. Uses the methods in routes.py (who use methods in censor + communication with the face detection engine)
 @app.route("/api/v1/detect", methods=["POST"])
 def detect():
     return detect_handler()
@@ -40,10 +46,12 @@ def detect():
 def censor():
     return censor_handler()
 
+# Automatically opens browser
 def open_browser():
     time.sleep(1)
     webbrowser.open("http://localhost:5001")
 
+# Opens server 
 if __name__ == "__main__":
     print("Bild-Verpixelungs-App startet...")
     print("Backend-Server auf http://localhost:5001")
